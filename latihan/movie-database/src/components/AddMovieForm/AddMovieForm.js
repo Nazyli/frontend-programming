92d891fc253c8edfa243040ac753b4e014313aaa
Evry Nazyli Ciptanto <evrynazyli@gmail.com>
2022-04-03 22:29:16 +0700

    Pertemuan 7

--- latihan/movie-database/src/components/AddMovieForm/AddMovieForm.js
+++ latihan/movie-database/src/components/AddMovieForm/AddMovieForm.js
@@ -1,5 +1,48 @@
+import { nanoid } from "nanoid";
+import { useState } from "react";
+import Alert from "../Alert/Alert";
 import styles from "./AddMovieForm.module.css"
-function AddMovieForm() {
+function AddMovieForm(props) {
+    const { movies, setMovies } = props;
+
+    // membuat state untuk title dan date
+
+    const [title, setTitle] = useState("");
+    const [date, setDate] = useState("");
+
+    // membuat state validasi
+    const [isTitleError, setIsTitleError] = useState(false);
+    const [isDateError, setIsDateError] = useState(false);
+
+    function handleTitle(e) {
+        setTitle(e.target.value);
+    }
+    function handleDate(e) {
+        setDate(e.target.value);
+    }
+    function handleSubmit(e) {
+        // mencegah perilaku default: refersh
+        e.preventDefault();
+        if (title === "") {
+            setIsTitleError(true)
+        } else if (date === "") {
+            setIsDateError(true);
+        } else {
+            const movie = {
+                id: nanoid(),
+                title: title,
+                year: date,
+                type: "Mobie",
+                poster: "https://picsum.photos/200/300?random=" + nanoid(2),
+            }
+
+            setMovies([...movies, movie])
+            setIsTitleError(false)
+            setIsDateError(false)
+        }
+
+    }
+
     return (
         <div className={styles.container}>
             <section className={styles.addmovie}>
@@ -13,11 +56,23 @@ function AddMovieForm() {
                 <div className={styles.addmovie__right}>
                     <div className={styles.addmovie_form}>
                         <h2 className={styles.addmovie__title}>Add Movie</h2>
-                        <form action="/handlelogin">
+                        <form onSubmit={handleSubmit}>
                             <label for="title" className={styles.addmovie__label}>Title</label><br />
-                            <input type="text" name="title" className={styles.addmovie__input} /><br />
+                            <input
+                                onChange={handleTitle}
+                                type="text"
+                                id="title"
+                                className={styles.addmovie__input}
+                                value={title} /><br />
+                            {isTitleError && <Alert>Title Wajib Diisi</Alert>}
                             <label for="year" className={styles.addmovie__label}>Year</label><br />
-                            <input type="text" name="year" className={styles.addmovie__input} /><br />
+                            <input
+                                onChange={handleDate}
+                                type="number "
+                                id="date"
+                                className={styles.addmovie__input}
+                                value={date} /><br />
+                            {isDateError && <Alert>Date Wajib Diisi</Alert>}
                             <input type="submit" value="Submit" className={styles.addmovie__submit} />
                         </form>
                     </div>
